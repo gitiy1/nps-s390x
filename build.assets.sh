bfdd31e14e2cc1a3ba7a596f6d3f088632cf5cd2
Git IY! <91715524+gitiy1@users.noreply.github.com>
2022-03-31 11:34:52 +0800

    Update build.assets.sh

--- build.assets.sh
+++ build.assets.sh
@@ -10,3 +10,6 @@ tar -czvf linux_s390x_client.tar.gz npc conf/npc.conf conf/multi_account.conf
 CGO_ENABLED=0 GOOS=linux GOARCH=s390x go build -ldflags "-s -w -extldflags -static -extldflags -static" ./cmd/nps/nps.go
 
 tar -czvf linux_s390x_server.tar.gz conf/nps.conf conf/tasks.json conf/clients.json conf/hosts.json conf/server.key  conf/server.pem web/views web/static nps
+
+cp –r linux_s390x_client.tar.gz nps_s390x 
+cp –r linux_s390x_server.tar.gz nps_s390x 
