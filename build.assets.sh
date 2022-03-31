43bb0702c2c6e51d452986e69f3a4f71e83d8c15
Git IY! <91715524+gitiy1@users.noreply.github.com>
2022-03-31 10:18:11 +0800

    Update build.assets.sh

--- build.assets.sh
+++ build.assets.sh
@@ -2,9 +2,6 @@ export GOPROXY=direct
 
 sudo apt-get update
 sudo apt-get install gcc-mingw-w64-i686 gcc-multilib
-env GOOS=windows GOARCH=386 CGO_ENABLED=1 CC=i686-w64-mingw32-gcc go build -ldflags "-s -w -extldflags -static -extldflags -static" -buildmode=c-shared -o npc_sdk.dll cmd/npc/sdk.go
-env GOOS=linux GOARCH=386 CGO_ENABLED=1 CC=gcc go build -ldflags "-s -w -extldflags -static -extldflags -static" -buildmode=c-shared -o npc_sdk.so cmd/npc/sdk.go
-tar -czvf npc_sdk.tar.gz npc_sdk.dll npc_sdk.so npc_sdk.h
 
 CGO_ENABLED=0 GOOS=linux GOARCH=s390x go build -ldflags "-s -w -extldflags -static -extldflags -static"  ./cmd/npc/npc.go
 
