f2f207bf71100bf76102cdbbd9b4137866adb47a
Git IY! <91715524+gitiy1@users.noreply.github.com>
2022-03-31 11:30:03 +0800

    Update build.assets.sh

--- build.assets.sh
+++ build.assets.sh
@@ -6,3 +6,7 @@ sudo apt-get install gcc-mingw-w64-i686 gcc-multilib
 CGO_ENABLED=0 GOOS=linux GOARCH=s390x go build -ldflags "-s -w -extldflags -static -extldflags -static"  ./cmd/npc/npc.go
 
 tar -czvf linux_s390x_client.tar.gz npc conf/npc.conf conf/multi_account.conf
+
+CGO_ENABLED=0 GOOS=linux GOARCH=s390x go build -ldflags "-s -w -extldflags -static -extldflags -static" ./cmd/nps/nps.go
+
+tar -czvf linux_s390x_server.tar.gz conf/nps.conf conf/tasks.json conf/clients.json conf/hosts.json conf/server.key  conf/server.pem web/views web/static nps
